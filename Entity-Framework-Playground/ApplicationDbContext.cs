8fef9f9c88701ea278c1c8121089bd5c6a45e6e7
Hesham236 <mohamed.hesham9780@gmail.com>
2022-09-21 14:55:22 +0200

    creating-removing migrations

---
+++ Entity-Framework-Playground/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Entity_Framework_Playground
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+        //HomeDatabase:
+        //options.UseSqlServer("");
+        //GehazDatabase:
+            options.UseSqlServer(@"Data Source = (localdb)\ProjectModels; Initial Catalog = EFCore; Integrated Security = True");
+            
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
--- Entity-Framework-Playground/Program.cs
+++ Entity-Framework-Playground/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Entity_Framework_Playground
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Entity_Framework_Playground
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var _context = new ApplicationDbContext();
+            var employee = new Employee ();
+
+            _context.Employees.Add(employee);
+
+            _context.SaveChanges();
         }
     }
 }
